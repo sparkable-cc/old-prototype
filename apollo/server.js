76599ac53c67d416cd6c115eb2c4cea73e91cb34
Patrick Venetz <venetz@gmail.com>
2021-05-07 19:54:05 +0200

    feat: Return user in resolver context

--- apollo/server.js
+++ apollo/server.js
@@ -2,25 +2,20 @@ const { ApolloServer } = require('apollo-server-express')
 const { buildContext } = require('graphql-passport')
 const { schema } = require('./schema')
 
-module.exports = (connections) => new ApolloServer({
-  schema,
-  context: function (context) {
-    return {
-      ...buildContext(context),
-      ...connections,
-      getConnectionIdentity: function (context) {
-        const sessionId = context.req?.sessionID
-        const accountId = context.getUser()?.id
-
-        if (!sessionId || !accountId) {
-          return undefined
-        }
-
-        return {
-          sessionId,
-          accountId
-        }
+module.exports = (connections) =>
+  new ApolloServer({
+    schema,
+    context: function (context) {
+      const grapqlContext = {
+        ...buildContext(context),
+        ...connections,
       }
-    }
-  }
-})
+
+      const user = grapqlContext.getUser()
+
+      return {
+        ...grapqlContext,
+        user,
+      }
+    },
+  })

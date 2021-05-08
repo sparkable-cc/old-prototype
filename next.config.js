904451ebf99b1991fb25dde6e772830553d6c589
Stefan N <stefnnn@users.noreply.github.com>
2021-05-08 07:35:08 +0200

    feat: add mdx support for markdown pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/,
+})
 const isDev = process.env.NODE_ENV !== 'production'
 const buildId = process.env.SOURCE_VERSION || ''
 
 console.log('next.config.js', { isDev, buildId })
 
-module.exports = {
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx', 'md'],
   poweredByHeader: false,
   generateBuildId: () => buildId,
   publicRuntimeConfig: {
     hash: buildId,
   },
-}
+})

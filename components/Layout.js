b85384fff6700ca10088c3222828e78811f180c5
Stefan N <stefnnn@users.noreply.github.com>
2021-05-08 09:52:12 +0200

    feat: content!!

--- components/Layout.js
+++ components/Layout.js
@@ -1,77 +1,14 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
-import {
-  Grid,
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Box,
-  Button,
-  Link,
-} from '@material-ui/core'
-import { Menu as MenuIcon, Add } from '@material-ui/icons'
-
-import { cdnify } from '../libs/utils'
-import withMe from '../libs/withMe'
-import AuthLogout from './Auth/Logout'
+import { Box } from '@material-ui/core'
 
 import styles from './Layout.module.css'
-import { useTheme } from '@material-ui/core'
-import { useRouter } from 'next/router'
+import TopBar from './commons/TopBar'
 
 const Layout = function (props) {
-  const theme = useTheme()
-  const router = useRouter()
-
   return (
     <>
-      <AppBar position="relative" color={theme.primary}>
-        <Toolbar>
-          <Box display="flex" flexGrow={1} alignItems="center">
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Box mr={4}>
-              <Typography variant="h6" color="inherit" noWrap>
-                <Link color="inherit" onClick={() => router.push('/')}>
-                  Butterfy
-                </Link>
-              </Typography>
-            </Box>
-            <img
-              src="/images/butterfy.png"
-              width="45"
-              height="40"
-              onClick={() => router.push('/')}
-            />
-          </Box>
-          {router.pathname != '/' && (
-            <Button color="inherit" onClick={() => router.push('/')}>
-              Start
-            </Button>
-          )}
-          {props.me ? (
-            <>
-              <Button color="inherit" onClick={() => router.push('/submit')}>
-                <Add /> Submit a Link
-              </Button>
-              <AuthLogout>
-                <Button color="inherit">Logout</Button>
-              </AuthLogout>
-            </>
-          ) : (
-            <>
-              <Button color="inherit" onClick={() => router.push('/login')}>
-                Login
-              </Button>
-              <Button color="inherit" onClick={() => router.push('/signup')}>
-                Signup
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
+      <TopBar />
       <Container maxWidth="md">
         <Box py={3}>{props.children}</Box>
       </Container>
@@ -79,4 +16,4 @@ const Layout = function (props) {
   )
 }
 
-export default withMe(Layout)
+export default Layout

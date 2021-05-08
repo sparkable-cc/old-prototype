a7603e816c73bddbfb6f4a09964a045b3605307a
Stefan N <stefnnn@users.noreply.github.com>
2021-05-08 11:05:12 +0200

    feat: implement voting and fake spam reporting

--- components/Submission.js
+++ components/Submission.js
@@ -1,23 +1,28 @@
 import { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import clsx from 'clsx'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardMedia from '@material-ui/core/CardMedia'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Collapse from '@material-ui/core/Collapse'
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
+import { Card, CardHeader, CardMedia, MenuItem } from '@material-ui/core'
+import { CardContent, CardActions } from '@material-ui/core'
+import { Collapse, Avatar, IconButton } from '@material-ui/core'
+import { Typography, Link, Menu } from '@material-ui/core'
 import { red } from '@material-ui/core/colors'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ShareIcon from '@material-ui/icons/Share'
+import { Favorite, Share, MoreVert } from '@material-ui/icons'
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-import Link from '@material-ui/core/Link'
 import { formatDate } from '../libs/date'
 import { useRouter } from 'next/router'
+import { gql, useMutation } from '@apollo/client'
+
+import { SubmissionFragment } from './Submit'
+
+const VOTE = gql`
+  mutation vote($submission_id: Int!) {
+    vote(submission_id: $submission_id) {
+      ...SubmissionFragment
+    }
+  }
+
+  ${SubmissionFragment}
+`
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -46,9 +51,19 @@ const Submission = ({ submission }) => {
   const router = useRouter()
   const classes = useStyles()
   const [hint, setHint] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [vote, { loading: voteLoading, error: voteError }] = useMutation(VOTE, {
+    variables: { submission_id: submission.id },
+  })
 
   const toggleHint = () => {
     setHint(!hint)
+  }
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const closeMenu = () => {
+    setAnchorEl(null)
   }
 
   return (
@@ -62,8 +77,8 @@ const Submission = ({ submission }) => {
           </Avatar>
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
+          <IconButton aria-label="settings" onClick={openMenu}>
+            <MoreVert />
           </IconButton>
         }
         title={
@@ -73,6 +88,16 @@ const Submission = ({ submission }) => {
         }
         subheader={formatDate(submission.date_posted)}
       />
+      <Menu
+        id="submission-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={closeMenu}
+      >
+        <MenuItem onClick={closeMenu}>Report as Spam</MenuItem>
+        <MenuItem onClick={closeMenu}>Report Copyright Infrigement</MenuItem>
+      </Menu>
       <Link href={submission.content.url}>
         <CardMedia
           className={classes.media}
@@ -91,15 +116,16 @@ const Submission = ({ submission }) => {
         <IconButton
           aria-label="add to favorites"
           onMouseOver={toggleHint}
+          onClick={vote}
           onMouseOut={toggleHint}
         >
-          <FavoriteIcon />
+          <Favorite />
         </IconButton>
         <IconButton aria-label="share">
           <Link
             href={`http://twitter.com/share?text=@ButterfyMe+reads+«${submission.content.title}»&url=${submission.content.url}`}
           >
-            <ShareIcon />
+            <Share />
           </Link>
         </IconButton>
         {/* <IconButton

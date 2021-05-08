6574bd7ee03a387840b55d484bc53406f77a0914
Patrick Venetz <venetz@gmail.com>
2021-05-08 11:33:30 +0200

    fix: Remove Submission.stage resolver
    Provided via database

--- apollo/resolvers.js
+++ apollo/resolvers.js
@@ -59,18 +59,6 @@ const resolvers = {
 
       return contents.findOne({ id: content_id })
     },
-    stage: async (parent, args, context, info) => {
-      const { id } = parent
-      const { pgdb } = context
-      const ballots = pgdb.public.ballots
-
-      const votes = await ballots.count({
-        submission_id: id,
-        vote: 'yes',
-      })
-
-      return getStage(votes)
-    },
     votes: async (parent, args, context, info) => {
       const { id } = parent
       const { pgdb } = context

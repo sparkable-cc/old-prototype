cc945419ef81ce410b46ebdf91e8ed1836fe2ddc
Stefan N <stefnnn@users.noreply.github.com>
2021-05-08 07:22:14 +0200

    feat: add call-to-action on startpage

--- components/StartUser.js
+++ components/StartUser.js
@@ -1,12 +1,30 @@
 import Submissions from './Submissions'
-import { Typography, Box } from '@material-ui/core'
+import { Typography, Box, Button } from '@material-ui/core'
+import { useRouter } from 'next/router'
+import { Container } from '@material-ui/core'
 const StartUser = (props) => {
+  const router = useRouter()
   return (
     <>
       <Box my={5}>
-        <Typography variant="h2">Meaningful Content for Us 🦋</Typography>
+        <Typography variant="h2">Meaningful Content for Us</Typography>
       </Box>
       <Submissions />
+      <Box my={10} />
+      <Container maxWidth="sm">
+        <Typography variant="h4" align="center">
+          <p>What is something meaningful you have read and want to share?</p>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => router.push('submit')}
+          >
+            Share a Link
+          </Button>
+        </Typography>
+      </Container>
+      <Box my={10} />
     </>
   )
 }

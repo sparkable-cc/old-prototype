27c90433f98fa76c643b3650a5094ac2fd50cd74
Stefan N <stefnnn@users.noreply.github.com>
2021-05-08 12:45:20 +0200

    feat: inline submit and signup

--- components/StartAnonymous.js
+++ components/StartAnonymous.js
@@ -1,17 +1,45 @@
 import { Typography, Container, Button, Box } from '@material-ui/core'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { Intro } from './StartUser'
+import AuthSignup from './Auth/Signup'
 
 const StartAnonymous = (props) => {
   const router = useRouter()
+  const [toggle, setToggle] = useState(false)
   return (
     <>
-      <Typography variant="h2">
-        <b>
-          Crowdfiltering the web. <br />
-          For meaningful content.
-        </b>
-        <br />
-        <br />
+      <Intro />
+      <Box mb={7}>
+        {toggle ? (
+          <AuthSignup />
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setToggle(!toggle)}
+          >
+            Sign Up Now
+          </Button>
+        )}
+      </Box>
+
+      <Typography variant="subtitle1">
+        <p>
+          Butterfy is a democratic collection of meaningful content. Sourced and
+          curated by the people, not by an algorithm or a newsroom. Here, you
+          decide in a 4-staged voting process together with everyone else what
+          content has the potential for impact. Looking for the signal within
+          the noise. We call that crowdfiltering.
+        </p>
+        <p>
+          Content that makes it through the 4-staged crowdfiltering transforms
+          from an Egg (1) into a Caterpillar (2), a Chrysalis (3) and finally
+          into a Butterfly. Or rather, a Butterfy (4).
+        </p>
+        <p>
+          Start by laying an Egg or exploring the Eggs others have laid already.
+        </p>
       </Typography>
       <Container maxWidth="sm">
         <img
@@ -21,26 +49,9 @@ const StartAnonymous = (props) => {
             border: '1px solid lightgray',
             padding: 16,
             background: 'white',
+            marginTop: 50,
           }}
         />
-        <Typography variant="subtitle1">
-          <p>
-            Butterfy is a democratic collection of meaningful content. Sourced
-            and curated by the people, not by an algorithm or a newsroom. Here,
-            you decide in a 4-staged voting process together with everyone else
-            what content has the potential for impact. Looking for the signal
-            within the noise. We call that crowdfiltering.
-          </p>
-          <p>
-            Content that makes it through the 4-staged crowdfiltering transforms
-            from an Egg (1) into a Caterpillar (2), a Chrysalis (3) and finally
-            into a Butterfly. Or rather, a Butterfy (4).
-          </p>
-          <p>
-            Start by laying an Egg or exploring the Eggs others have laid
-            already.
-          </p>
-        </Typography>
       </Container>
 
       <Box my={8}>
--- components/StartUser.js
+++ components/StartUser.js
@@ -4,43 +4,28 @@ import { useRouter } from 'next/router'
 import { Container } from '@material-ui/core'
 import styles from './StartUser.module.css'
 import { useEffect, useState } from 'react'
+import Submit from './Submit'
 
 const StartUser = (props) => {
   const router = useRouter()
-  const [mix, setMix] = useState(0)
-
-  useEffect(() => {
-    setTimeout(nextRotate, 2000)
-  })
-
-  const nextRotate = () => {
-    setTimeout(() => {
-      setMix(mix < meaningful.length - 1 ? mix + 1 : 0)
-    }, 2000)
-  }
+  const [toggle, setToggle] = useState(false)
 
   return (
     <>
-      <Box my={5} maxWidth="sm" style={{ maxWidth: 600 }}>
-        <Typography variant="h2" gutterBottom={40}>
-          Burst your filter bubble and change the flow of media
-        </Typography>
-        <Typography variant="h4">
-          Submit the most &nbsp;{' '}
-          <span class={styles.roto}>{meaningful[mix]}</span> &nbsp; content you
-          have encountered recently
-          <p>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => router.push('submit')}
-            >
-              Share a Link
-            </Button>
-          </p>
-        </Typography>
+      <Intro />
+      <Box mb={7}>
+        {toggle ? (
+          <Submit />
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setToggle(!toggle)}
+          >
+            Share a Link
+          </Button>
+        )}
       </Box>
-
       <Typography variant="h4">Discover what other people sent in</Typography>
       <Submissions />
     </>
@@ -64,5 +49,31 @@ const meaningful = [
   'touching',
   'enriching',
 ]
+
+export const Intro = () => {
+  const [mix, setMix] = useState(0)
+
+  useEffect(() => {
+    setTimeout(nextRotate, 2000)
+  })
+
+  const nextRotate = () => {
+    setTimeout(() => {
+      setMix(mix < meaningful.length - 1 ? mix + 1 : 0)
+    }, 2000)
+  }
+  return (
+    <Box my={5} maxWidth="sm" style={{ maxWidth: 600 }}>
+      <Typography variant="h2" gutterBottom={40}>
+        Burst your filter bubble and change the flow of media
+      </Typography>
+      <Typography variant="h4">
+        Submit the most &nbsp;{' '}
+        <span class={styles.roto}>{meaningful[mix]}</span> &nbsp; content you
+        have encountered recently
+      </Typography>
+    </Box>
+  )
+}
 
 export default StartUser
